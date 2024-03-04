f349e06c13a9a40238a363330817c5b5c8ff2959
vitaliy <vitalyas-zarochintsev@yandex.ru>
2024-03-05 03:25:41 +0700

    print odd or event numbers

---
+++ OddEven/OddEven/OddEven.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#define N 15
+
+void PrintOddOrEvenNumbers(int maxNumber, bool isOdd);
+
+int main()
+{
+    PrintOddOrEvenNumbers(N, true);
+    PrintOddOrEvenNumbers(N, false);
+}
+
+void PrintOddOrEvenNumbers(int maxNumber, bool isOdd)
+{
+    for (int i = 0; i < maxNumber + 1; i++)
+    {
+        bool isOddNumber = i % 2 == 0;
+
+        if (isOdd && isOddNumber)
+        {
+            std::cout << i << " ";
+        }
+        else if (!isOdd && !isOddNumber)
+        {
+            std::cout << i << " ";
+        }
+    }
+
+    std::cout << std::endl;
+}
